176ce5a1419b5b614370bcf736523dec0573269c
Van Vien Dong <vanviendong96@gmail.com>
2021-11-01 00:28:00 +0700

    Components Part 1

--- GettingStartedReact/my-app/src/App.js
+++ GettingStartedReact/my-app/src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          {/* LAB03 */}
-        </div>
-      </Navbar>
+      <Menu />
     </div>
   );
 }
