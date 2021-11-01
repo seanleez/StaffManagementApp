0dd353f4ba23165d83b70c22307467d2c8e89c86
Van Vien Dong <vanviendong96@gmail.com>
2021-11-02 01:18:01 +0700

    Component Part 2

--- GettingStartedReact/my-app/src/App.js
+++ GettingStartedReact/my-app/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes'
 
-function App() {
-  return (
-    <div>
-      <Menu />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes : DISHES
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Menu dishes = {this.state.dishes}/>
+      </div>
+    );
+  }
 }
 export default App;
