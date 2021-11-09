b7c889a49af3c0d2affc56aae131494e8aaf3e2d
Van Vien Dong <vanviendong96@gmail.com>
2021-11-09 21:51:13 +0700

    Fulfill all requirement

--- src/App.js
+++ src/App.js
@@ -3,17 +3,38 @@ import './App.css';
 import StaffList from './components/StaffComponent';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import { STAFFS } from './shared/staffs';
-import { OPTIONS } from './shared/options'
+// import { OPTIONS } from './shared/options'
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			staffs: STAFFS,
-            responsiveCol: "col-12 col-md-6 col-lg-4"
+            responsiveCol: ""
 		};
         this.handleChange = this.handleChange.bind(this);
 	}
+
+    // After component did mount, add eventlistener to catch resize of window
+    // Whenever width of window satisfy below condition, we setState of this Component, 
+    // this will effect change selected value of [SELECT]
+    // and transfer responsiveCol to StaffList as a props
+    componentDidMount() {
+        window.addEventListener("resize", this.resize.bind(this));
+        this.resize();
+    }
+    resize() {
+        if (window.innerWidth >= 992 ) {
+            this.setState({ responsiveCol : "col-12 col-md-6 col-lg-4"});
+        } else if (window.innerWidth < 992 && window.innerWidth >= 768) {
+            this.setState({ responsiveCol : "col-12 col-md-6" })
+        } else {
+            this.setState({ responsiveCol : "col-12"})
+        }
+    }
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.resize.bind(this));
+    }
 
     handleChange(e) {
         this.setState({ responsiveCol: e.target.value });
@@ -30,9 +51,12 @@ class App extends Component {
 				</Navbar>
                 <div className="container">
                     <select value={this.state.responsiveCol} onChange={this.handleChange}>
-                        {OPTIONS.map((option) => (
+                        {/* {OPTIONS.map((option) => (
                             <option value={option.value}>{option.label}</option>
-                        ))}
+                        ))} */}
+                        <option value="col-12 col-md-6 col-lg-4">Three Columns</option>
+                        <option value="col-12 col-md-6">Two Columns</option>
+                        <option value="col-12">One Column</option>
                     </select>
                 </div>
 				<StaffList staffs={this.state.staffs} responsiveCol={this.state.responsiveCol} />
