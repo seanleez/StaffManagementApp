3c891b6209c23648a917ad6e35f018d275b5380d
Van Vien Dong <vanviendong96@gmail.com>
2021-11-18 01:01:20 +0700

    Finish Sort Function

--- src/components/SalaryComponent.js
+++ src/components/SalaryComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Card, CardTitle, CardText, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
@@ -17,31 +17,64 @@ function RenderSalaryItem({salaryItem}) {
     )
 }
 
-const Salary = (props) => {
-    const salaryItemList = props.salaryItems.map((salaryItem) => {
+class Salary extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            sortOrder: "nameAscending"
+        }
+        this.handleSortChange = this.handleSortChange.bind(this)
+    }
+
+    handleSortChange(e) {
+        this.setState({ sortOrder: e.target.value})
+    }
+
+    render() {
+        // Use slice method for clone an array
+        var sortList = this.props.salaryItems.slice();
+        if (this.state.sortOrder === "salaryAscending") {
+            sortList.sort((a,b) => (a.salary > b.salary) ? 1 : ((b.salary > a.salary) ? -1 : 0))
+        } else if (this.state.sortOrder === "salaryDescending") {
+            sortList.sort((a,b) => (a.salary < b.salary) ? 1 : ((b.salary < a.salary) ? -1 : 0))
+        } else if (this.state.sortOrder === "nameAscending"){
+            sortList.sort((a,b) => (a.name.split(" ").at(-1) > b.name.split(" ").at(-1)) ? 1 : ((b.name.split(" ").at(-1) > a.name.split(" ").at(-1)) ? -1 : 0))
+        } else {
+            sortList.sort((a,b) => (a.name.split(" ").at(-1) < b.name.split(" ").at(-1)) ? 1 : ((b.name.split(" ").at(-1) < a.name.split(" ").at(-1)) ? -1 : 0))
+        }
+
+        const salaryItemList = sortList.map((salaryItem) => {
+            return(
+                <div key={salaryItem.id} className="col-12 col-sm-6 col-lg-4 p-2">
+                    <RenderSalaryItem salaryItem={salaryItem} />
+                </div>
+            );
+        })
         return(
-            <div key={salaryItem.id} className="col-12 col-sm-6 col-lg-4 p-2">
-                <RenderSalaryItem salaryItem={salaryItem} />
-            </div>
-        );
-    })
-    return(
-        <div className="container mb-3">
-            <div className="row mt-2">
-                <Breadcrumb>
-                    <BreadcrumbItem><Link to="/staff">Staff</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>Salary</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>Salary</h3>
-                    <hr />
+            <div className="container mb-3">
+                <div className="row mt-2">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/staff">Staff</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Salary</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Salary</h3>
+                        <hr />
+                    </div>
+                </div>
+                <select onChange={this.handleSortChange} className="mb-2">
+                    <option defaultValue hidden>Sort by</option>
+                    <option value="nameAscending">Name: Ascending</option>
+                    <option value="nameDescending">Name: Descending</option>
+                    <option value="salaryAscending">Salary: Ascending</option>
+                    <option value="salaryDescending">Salary: Descending</option>
+                </select>
+                <div className="row">
+                    {salaryItemList}
                 </div>
             </div>
-            <div className="row">
-                {salaryItemList}
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Salary
--- src/components/StaffComponent.js
+++ src/components/StaffComponent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Card, CardImg, CardTitle } from 'reactstrap'
 import { Link } from 'react-router-dom';
 
@@ -13,28 +13,52 @@ function RenderStaffItem({staff}) {
     )
 }
 
-const StaffList = (props) => {
-    const staffList = props.staffs.map((staff) => {
+class StaffList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sortOrder: "nameAscending"
+        }
+        this.handleSortChange = this.handleSortChange.bind(this);
+    }
+
+    handleSortChange(e) {
+        this.setState({ sortOrder: e.target.value });
+    } 
+
+    render() {
+        var sortList = this.props.staffs.slice()
+        if (this.state.sortOrder === "nameAscending") {
+            sortList.sort((a,b) => (a.name.split(" ").at(-1) > b.name.split(" ").at(-1)) ? 1 : ((b.name.split(" ").at(-1) > a.name.split(" ").at(-1)) ? -1 : 0))
+        } else {
+            sortList.sort((a,b) => (a.name.split(" ").at(-1) < b.name.split(" ").at(-1)) ? 1 : ((b.name.split(" ").at(-1) < a.name.split(" ").at(-1)) ? -1 : 0))
+        }
+        const staffList = sortList.map((staff) => {
+            return(
+                <div key={staff.id} className="col-12 col-sm-4 col-lg-2 my-2 p-2">
+                    <RenderStaffItem staff={staff}/>
+                </div>
+            )
+        });
         return(
-            <div key={staff.id} className="col-12 col-sm-4 col-lg-2 my-2 p-2">
-                <RenderStaffItem staff={staff}/>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 mt-2">
+                        <h3>Staff</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <select onChange={this.handleSortChange}>
+                    <option defaultValue hidden>Sort by</option>
+                    <option value="nameAscending">Name: Ascending</option>
+                    <option value="nameDescending">Name: Descending</option>
+                </select>
+                <div className="row">
+                    {staffList}
+                </div>
             </div>
         )
-    });
-
-    return(
-        <div className="container">
-            <div className="row">
-                <div className="col-12 mt-2">
-                    <h3>Staff</h3>
-                    <hr />
-                </div>                
-            </div>
-            <div className="row">
-                {staffList}
-            </div>
-        </div>
-    )
+    }
 }
 
 export default StaffList
